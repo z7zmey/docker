6c0f02e22bc0724e54a41e411edcf38a35778105
z7zmey <z7zmey@gmail.com>
2016-01-05 14:12:29 +0200

    couchbase-sync-gateway run.sh fix

--- couchbase-sync-gateway/latest/run.sh
+++ couchbase-sync-gateway/latest/run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 function finish {
-  kill -9 `pidof gearmand`
+	kill -9 `pidof sync_gateway`
 }
 
 trap finish SIGTERM SIGKILL
 
 if [ ! -z $COUCHBASE_HOST ]; then
-  for i in {60..0}; do
-    wget -S --spider $COUCHBASE_HOST:${COUCHBASE_PORT:-8091} 2>&1 | awk '/^  /'
-    if [ $? -ne 0 ] then
-      break
-    fi
+	for i in {30..0}; do
+		if $(curl --output /dev/null --silent --head --fail $COUCHBASE_HOST:8091/pools/); then
+		  break
+		fi
 
 		echo 'Couchbase init process in progress...'
 		sleep 2
@@ -21,6 +20,9 @@ if [ ! -z $COUCHBASE_HOST ]; then
 		echo >&2 'Couchbase init process failed.'
 		exit 1
 	fi
+
+	echo 'Couchbase init process success. Run Sync gateway after 40s'
+	sleep 40
 fi
 
 exec sync_gateway /config.json
