3ff59b51fcb4f4cd7772050804fe69a3e6eace14
z7zmey <z7zmey@gmail.com>
2017-07-02 14:32:24 +0300

    ?

--- rabbitmq-cli-consumer-php/php70/docker-entrypoint.sh
+++ rabbitmq-cli-consumer-php/php70/docker-entrypoint.sh
@@ -5,4 +5,4 @@ if [ "$1" = '/bin/bash' ]; then
     exec "$@"
 fi
 
-exec rabbitmq-cli-consumer -c /etc/rabbitmq-cli-consumer/config.conf -V -e "$@"
+exec rabbitmq-cli-consumer -c ${CONFIG_PATH:-/etc/rabbitmq-cli-consumer/config.conf} -V -e "$@"
